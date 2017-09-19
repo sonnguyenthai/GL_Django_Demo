b55647ea8a8085c9c7d0ff1fc88ccde32eaf19f6
Nguyen Thai Son <sonnt@Admins-MacBook-Air.local>
2017-09-19 22:00:20 +0200

    initial code

--- gldemo/admin.py
+++ gldemo/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import Book
 
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'date_published')
+
+admin.site.register(Book, BookAdmin)
